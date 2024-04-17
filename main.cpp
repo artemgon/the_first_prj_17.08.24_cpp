327f9c314a23118b08ef1ede17fe7e02c0b21f15
artemgon <artemhoncharuk123@gmail.com>
2024-04-17 20:48:45 +0300

    Creating the first task.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main() {
+	cout << "\tName\t\t:" << "\"The war and peace\"\n";
+	cout << "\tAvtor\t\t:" << "    L.N.Tolstoj\n";
+	cout << "\tIzdatelstvo\t:" << "  Piter\n";
+	cout << "\tPages\t\t:" << " 500";
 	return 0;
 }
