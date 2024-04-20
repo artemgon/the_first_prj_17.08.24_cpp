0f22affc092473b1d2fbb67371cb2d05f3ab66f9
artemgon <artemhoncharuk123@gmail.com>
2024-04-20 22:16:34 +0300

    Creating the first task of homework.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main() {
-	cout << "\tName\t\t:" << "\"The war and peace\"\n";
-	cout << "\tAvtor\t\t:" << "    L.N.Tolstoj\n";
-	cout << "\tIzdatelstvo\t:" << "  Piter\n";
-	cout << "\tPages\t\t:" << " 500";
+	float first_resistance;
+	cout << "Please, enter the value of R1:";
+	cin >> first_resistance;
+	float second_resistance;
+	cout << "Please, enter the value of R2:";
+	cin >> second_resistance;
+	float third_resistance;
+	cout << "Please, enter the value of R3:";
+	cin >> third_resistance;
+	float seroth_resistance = 1 / first_resistance + 1 / second_resistance + 1 / third_resistance;
+	cout << "R0 = " << seroth_resistance;
 	return 0;
 }
