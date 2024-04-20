b1935dc3f1bb1eea2cdda53402fcb36eb2277c67
artemgon <artemhoncharuk123@gmail.com>
2024-04-20 20:36:51 +0300

    Creating the first task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main() {
+	cout << "\tName\t\t:" << "\"The war and peace\"\n";
+	cout << "\tAvtor\t\t:" << "    L.N.Tolstoj\n";
+	cout << "\tIzdatelstvo\t:" << "  Piter\n";
+	cout << "\tPages\t\t:" << " 500";
+	cout << "Let's play!\n";
 	return 0;
 }
