690424d5cc4f32949528b6594987975489cea1c3
artemgon <artemhoncharuk123@gmail.com>
2024-04-20 21:57:33 +0300

    Creating the third task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <iomanip> 
 using namespace std;
 
 int main() {
+	char lt = static_cast<char>(201);
+	char lb = static_cast<char>(200);
+	char rt = static_cast<char>(187);
+	char rb = static_cast<char>(188);
+	char h = static_cast<char>(205);
+	char v = static_cast<char>(186);
+	char uvhm = static_cast<char>(204);
+	char uhvm = static_cast<char>(185);
+	char uhvb = static_cast<char>(203);
+	char uvhb = static_cast<char>(202);
+	cout << lt << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << rt << "\n" << v << "\t\t\t\t\t\t\t\t\t" << v << "\n" << v << "\t\t\t\tVremena goda" << "\t\t\t\t" << v << "\n" << v << "\t\t\t\t\t\t\t\t\t" << v << "\n" << uvhm << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << uhvb << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << uhvb << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << uhvb << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << uhvm << "\n" << v << setw(18) << v << setw(18) << v << setw(20) << v << setw(16) << v << "\n" << v << setw(11) << "Zima" << setw(7) << v << setw(11) << "Vesna" << setw(7) << v << setw(12) << "Leto" << setw(8) << v << setw(10) << "Osen" << setw(6) << v << "\n" << v << setw(18) << v << setw(18) << v << setw(20) << v << setw(16) << v << "\n" << lb << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << uvhb << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << uvhb << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << uvhb << h << h << h << h << h << h << h << h << h << h << h << h << h << h << h << rb;
 	return 0;
 }
