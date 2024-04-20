bed38e196b83348088aa169ce5e9c716791b4942
artemgon <artemhoncharuk123@gmail.com>
2024-04-20 20:44:52 +0300

    Creating the second task of practice exercises.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 6);
+	cout << "\"U lukomor'a dub zelenij,\n" << "Zlataya zep na dupe tom,\n" << "I dnem i nochju kot uchenij\n" << "I dnem i nochju kot uchenij\n" << "vse hodit po cepi krugom\"";
 	return 0;
 }
