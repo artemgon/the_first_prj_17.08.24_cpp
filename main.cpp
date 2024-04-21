f4b3ccb46547378cd8b7a9cc4122d24195dbc5bd
artemgon <artemhoncharuk123@gmail.com>
2024-04-21 19:01:18 +0300

    Creating the third task of homework.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
+	float initial_velocity;
+	cout << "Please, enter the initial velocity:";
+	cin >> initial_velocity;
+	float time;
+	cout << "Please, enter the time:";
+	cin >> time;
+	float acceleration;
+	cout << "Please, enter the acceleration:";
+	cin >> acceleration;
+	float displacement = initial_velocity * time + (acceleration * pow(time, 2)) / 2;
+	cout << "The displacement = " << displacement;
 	return 0;
 }
