ff2ded4565ff765818840be2691e46c5c50db251
artemgon <artemhoncharuk123@gmail.com>
2024-04-21 18:43:42 +0300

    Creating the second task of homework.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
+	const float pi = 3.14;
+	float circumference;
+	cout << "Please, enter the circumference:";
+	cin >> circumference;
+	float radius = circumference / (2 * pi);
+	float circle_area = pi * pow(radius, 2);
+	cout << "Area of circle = " << circle_area;
 	return 0;
 }
